b5a08c9c502e988ad18fda0951b78be3a3158c16
unknown <mutungaeric839@gmail.com>
2024-02-09 14:10:00 +0300

    updating the homepage for input

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,42 @@
 <template>
-  <div class="home">
-    <h1>Hey Beutiful!</h1>
-    <p>Whats your name?</p>
-    
-    
+  <div>
+    <h1>Home Page</h1>
+    <input type="text" v-model="enteredName" placeholder="Enter your name"/>
+    <button @click="checkName">Enter</button>
+    <div v-if="showError" class="error-popup">
+      You are not Dorcas!
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  
- 
+  data() {
+    return {
+      enteredName: '',
+      showError: false,
+    };
+  },
+  methods: {
+    checkName() {
+      // Convert both input and comparison string to lowercase to ensure case-insensitive comparison
+      if (this.enteredName.toLowerCase() === 'dorcas') {
+        this.$router.push({ name: 'name' }); // Assuming 'name' is the name of the route you want to navigate to
+      } else {
+        this.showError = true;
+        // Optionally hide the popup after some time
+        setTimeout(() => {
+          this.showError = false;
+        }, 3000);
+      }
+    }
+  }
 };
 </script>
 
 <style scoped>
-.home {
-  text-align: center;
-  padding: 20px;
+.error-popup {
+  color: red;
 }
-
-
 </style>
