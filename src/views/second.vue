6462d5cdb19a35663f5e018cafaa38953d9f1285
unknown <mutungaeric839@gmail.com>
2024-02-09 11:57:47 +0300

    add font

--- src/views/second.vue
+++ src/views/second.vue
@@ -1,20 +1,22 @@
 <template>
     <div class="dorcas-container">
-      <h2 class="greeting">🌹Fun Fact about the name Dorcas!🌹</h2>
-      <p class="message">
-        Dorcas is a name of Greek origin. In the Bible Dorcas is a warm woman who is known for her charitable acts and good works. She is also referred to as Tabitha, which is an Aaramaic name. It is a name that was popular in the early Christian church.
-      </p>
-      <button @click="showSurprise">Click for a Surprise</button>
-        <div v-if="surprise">
-            <div>
-                <h1>Happy Valentines Babe!❤️</h1>
-            </div>
-            <div class="gallery">
-                <img src="@/assets/F1.png" alt="Love" />
+        <h2 class="greeting">🌹Fun Fact about the name Dorcas!🌹</h2>
+        <p class="message">
+            Dorcas is a name of Greek origin. In the Bible, Dorcas is a woman known for her charitable acts and good works. She is also referred to as Tabitha, which is an Aramaic name. It is a name that was popular in the early Christian church.
+        </p>
+        <button @click="showSurprise">Click for a Surprise</button>
+        <div v-if="surprise" class="modal-overlay" @click.self="surprise = false">
+            <div class="modal">
+                <h1 class="h1-valentine">Happy Valentine's Babe!❤️</h1>
+                <div class="gallery">
+                    <img src="@/assets/F1.png" alt="Love" />
+                </div>
+                <button @click="surprise = false">Close</button>
             </div>
         </div>
     </div>
 </template>
+
   
 
 <script>
@@ -45,6 +47,12 @@ export default {
   border-radius: 15px;
   color: #333;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+}
+
+.h1-valentine {
+  font-family: 'Great Vibes', cursive;
+  font-size: 48px; /* Adjust the size as needed */
+  color: #e63946; /* Feel free to change the color */
 }
 
 .greeting {
@@ -83,6 +91,7 @@ export default {
 }
 
 button {
+  margin-top: 20px;
   padding: 10px 20px;
   font-size: 16px;
   color: #fff;
@@ -97,5 +106,30 @@ button {
 button:hover {
   background-color: #ff6b6b;
 }
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  max-width: 500px;
+  height: 97%;
+  text-align: center;
+  
+}
+
+/* Add or modify button styles here if necessary */
+
 
 </style>
