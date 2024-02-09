9a7c998b09947d3cc22195ee3c94586e2af272c9
unknown <mutungaeric839@gmail.com>
2024-02-09 10:51:39 +0300

    adding flower image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,42 @@
-<!-- src/views/Home.vue -->
 <template>
-    <div>
-      <h1>Home Page</h1>
+  <div class="home">
+    <h1>Hey Beutiful!</h1>
+    <p>Whats your name?</p>
+    
+    <button @click="showSurprise">Click for a Surprise</button>
+    <div v-if="surprise">
+      <div class="gallery">
+        <img src="@/assets/F1.png" alt="Love" />
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Home'
-  }
-  </script>
-  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      surprise: false,
+    };
+  },
+  methods: {
+    showSurprise() {
+      this.surprise = !this.surprise;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.gallery img {
+  max-width: 100%;
+  height: auto;
+  border-radius: 8px;
+}
+</style>
